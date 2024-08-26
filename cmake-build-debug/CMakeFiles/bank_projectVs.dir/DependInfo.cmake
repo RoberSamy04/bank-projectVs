ed2dabb6a8453e87acf4819d602abc1a6cb2b2bd
RoberSamy04 <robersamy04@gmail.com>
2024-08-26 15:22:48 +0300

    upDateCmake

---
+++ cmake-build-debug/CMakeFiles/bank_projectVs.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "F:/bank projectVs/Admin.cpp" "CMakeFiles/bank_projectVs.dir/Admin.cpp.obj" "gcc" "CMakeFiles/bank_projectVs.dir/Admin.cpp.obj.d"
+  "F:/bank projectVs/Client.cpp" "CMakeFiles/bank_projectVs.dir/Client.cpp.obj" "gcc" "CMakeFiles/bank_projectVs.dir/Client.cpp.obj.d"
+  "F:/bank projectVs/Employee.cpp" "CMakeFiles/bank_projectVs.dir/Employee.cpp.obj" "gcc" "CMakeFiles/bank_projectVs.dir/Employee.cpp.obj.d"
+  "F:/bank projectVs/Person.cpp" "CMakeFiles/bank_projectVs.dir/Person.cpp.obj" "gcc" "CMakeFiles/bank_projectVs.dir/Person.cpp.obj.d"
+  "F:/bank projectVs/Validation.cpp" "CMakeFiles/bank_projectVs.dir/Validation.cpp.obj" "gcc" "CMakeFiles/bank_projectVs.dir/Validation.cpp.obj.d"
+  "F:/bank projectVs/main.cpp" "CMakeFiles/bank_projectVs.dir/main.cpp.obj" "gcc" "CMakeFiles/bank_projectVs.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/bank_projectVs.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bank_projectVs.dir/Admin.cpp.obj"
+  "CMakeFiles/bank_projectVs.dir/Admin.cpp.obj.d"
+  "CMakeFiles/bank_projectVs.dir/Client.cpp.obj"
+  "CMakeFiles/bank_projectVs.dir/Client.cpp.obj.d"
+  "CMakeFiles/bank_projectVs.dir/Employee.cpp.obj"
+  "CMakeFiles/bank_projectVs.dir/Employee.cpp.obj.d"
+  "CMakeFiles/bank_projectVs.dir/Person.cpp.obj"
+  "CMakeFiles/bank_projectVs.dir/Person.cpp.obj.d"
+  "CMakeFiles/bank_projectVs.dir/Validation.cpp.obj"
+  "CMakeFiles/bank_projectVs.dir/Validation.cpp.obj.d"
+  "CMakeFiles/bank_projectVs.dir/main.cpp.obj"
+  "CMakeFiles/bank_projectVs.dir/main.cpp.obj.d"
+  "bank_projectVs.exe"
+  "bank_projectVs.exe.manifest"
+  "bank_projectVs.pdb"
+  "libbank_projectVs.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bank_projectVs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
